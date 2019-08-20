339545ec0e6f800d7468a0ca25fc937b2cd41669
leiyu <18924260620@163.com>
2019-08-20 23:47:16 +0800

    add event to BraftEditor

--- src/pages/Article/index.tsx
+++ src/pages/Article/index.tsx
@@ -94,64 +94,6 @@ class ArticleComponent extends React.Component<ArticleComponentProps> {
     }
 }
 
-// const ArticleComponent: React.FC<ArticleComponentProps> = props => {
-//     const columns = [
-//         {
-//           title: "标题",
-//           dataIndex: "title",
-//           key: "title",
-//         },
-//         {
-//           title: "作者",
-//           dataIndex: "author",
-//           key: "author",
-//         },
-//         {
-//           title: "分类",
-//           dataIndex: "category",
-//           key: "category",
-//         },
-//         {
-//             title: "发布时间",
-//             dataIndex: "publish_at",
-//             key: "publish_at"
-//         },
-//         {
-//             title: '操作',
-//             key: 'action',
-//             render: (text, record) => (
-//               <span>
-//                 <a href="">发布</a>
-//                 <Divider type="vertical" /> 
-//                 <a href="javascript:;">查看 {record.name}</a>
-//                 <Divider type="vertical" />
-//                 <a href="javascript:;">删除</a>
-//               </span>
-//             ),
-//         },
-//     ];
-//     const dataSource = [
-//         {
-//             "key": 1,
-//             "title": "tomato1.0 系统",
-//             "author": "leiyu",
-//             "category": "编程",
-//             "publish_at": "2019-08-03"
-//         }
-//     ]
-//     return (
-//         <div>
-//             <SearchForm
-//                 handleSubmit={() => {console.log(".......")}}
-//             />
-//             <Table 
-//                 columns={columns}
-//                 dataSource={dataSource}
-//             />
-//         </div>
-//     )
-// }
-
 export default connect(({ article }: ConnectState) => ({
     article,
   }))(ArticleComponent);
--- src/pages/Editor/index.tsx
+++ src/pages/Editor/index.tsx
@@ -8,12 +8,55 @@ import "braft-editor/dist/index.css";
 import { Input } from "antd"
 
 class Editor extends React.Component<any> {
-    /** markdown编辑器 */
+    /** 
+     * markdown编辑器 
+     * Usage see: https://www.yuque.com/braft-editor/be/mrgy92
+     */
+    constructor(props) {
+        super(props)
+
+        this.state = {
+            contextValue: String
+        }
+    }
+    // 存为草稿
+    handleSaveToDraft = (e) => {
+        // 保存文本
+    }
     render() {
+
+        const extendControls = [
+            "separator",
+            {
+                key: "btn-save", // 控件唯一标识，必传
+                type: "button",
+                title: "文章保存到草稿", // 指定鼠标悬停提示文案
+                className: "my-button", // 指定按钮的样式名
+                html: "<strong>存为草稿</strong>", // 指定在按钮中渲染的html字符串
+                text: "存为草稿",
+                onClick: () => {
+                    console.log("Hello World!");
+                },
+            },
+            "separator",
+            {
+                key: "btn-publish", // 控件唯一标识，必传
+                type: "button",
+                title: "发布文章", // 指定鼠标悬停提示文案
+                className: "my-button", // 指定按钮的样式名
+                html: "<strong>发布文章</strong>", // 指定在按钮中渲染的html字符串
+                text: "发 布", // 指定按钮文字，此处可传入jsx，若已指定html，则text不会显示
+                onClick: () => {
+                    console.log("Hello World!");
+                },
+            }
+        ]
         return (
             <div>
                 <Input placeholder="文章标题"/>
-                <BraftEditor />
+                <BraftEditor 
+                    extendControls={extendControls}
+                />
             </div>
         )
     }
